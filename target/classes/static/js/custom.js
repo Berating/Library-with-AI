0be4944badd7fdce56c9d7d06677aff5a0688f4f
Berating <59777519+Berating@users.noreply.github.com>
2024-04-16 13:48:46 +0300

    Add files via upload

---
+++ target/classes/static/js/custom.js
@@ -0,0 +1,37 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// owl carousel 
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+function showAddBookModal() {
+    $("#addBookModal").modal("show");
+}
+
+function hideAddBookModal() {
+    $("#addBookModal").modal("hide");
+}
